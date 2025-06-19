8feee68f6f3d4fe3f749734bfddb7cb7ffaa07e7
Alicia <aliciajuillet0409@gmail.com>
2025-06-19 15:01:17 +0200

    contact-ok

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -4,14 +4,20 @@ function Contact() {
     return(
         <section id="contact">
             <h2>Me contacter</h2>
-            <div className="contact-item">
-                <i className="fa-solid fa-mobile-screen"></i>
-                <p>+33 6 73 66 11 83</p>
+            <div className="contact-content">
+                <div className="contact-item">
+                    <i className="fas fa-envelope"></i>
+                    <p>aliciajuillet0409@gmail.com</p>
+                </div>
+                <div className="contact-item">
+                    <i className="fa-solid fa-mobile-screen"></i>
+                    <p>+33 6 73 66 11 83</p>
+                </div>
+                <div className="contact-item">
+                    <i class="fa-brands fa-linkedin"></i>
+                    <a href="linkedin">www.linkedin.com/in/alicia-juillet</a>
+                </div>
             </div>
-              <div className="contact-item">
-                <i className="fas fa-envelope"></i>
-                <p>aliciajuillet0409@gmail.com</p>
-  </div>
         </section>
     )
 }
