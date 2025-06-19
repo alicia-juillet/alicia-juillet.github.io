38917db4dbfbe39828ee1a98ace7de19b1dbeed4
Alicia <aliciajuillet0409@gmail.com>
2025-06-19 14:47:16 +0200

    skills-ok

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -7,37 +7,42 @@ function Skills() {
         <section id="skills">
             <h2>Mes compétences</h2>
             <div className="skills-category">
-                <h3>Frontend</h3>
-                <div className="skills-icons">
-                    <i className="devicon-html5-plain colored" title="HTML5"></i>
-                    <i className="devicon-css3-plain colored"title="CSS3"></i>
-                    <i className="devicon-javascript-plain colored"title="JavaScript"></i>
-                    <i className="devicon-react-plain colored"title="React"></i> 
-                    <i className="devicon-sass-plain colored" title="SASS"></i>  
+                <div className="skills-content">
+                    <h3>Frontend</h3>
+                    <div className="skills-icons">
+                        <i className="devicon-html5-plain colored" title="HTML5"></i>
+                        <i className="devicon-css3-plain colored"title="CSS3"></i>
+                        <i className="devicon-javascript-plain colored"title="JavaScript"></i>
+                        <i className="devicon-react-plain colored"title="React"></i> 
+                        <i className="devicon-sass-plain colored" title="SASS"></i>  
+                    </div>
                 </div>
-            </div>
-            <div className="skills-category">
-                <h3>Backend</h3>
-                <div className="skills-icons">
-                    <i className="devicon-nodejs-plain colored" title="Node.js"></i>
-                    <i className="devicon-express-original colored" title="Express.js"></i>
-                    <i className="devicon-mongodb-plain colored" title="MongoDB"></i>
+           
+                <div className="skills-content">
+                    <h3>Backend</h3>
+                    <div className="skills-icons">
+                        <i className="devicon-nodejs-plain colored" title="Node.js"></i>
+                        <i className="devicon-express-original colored" title="Express.js"></i>
+                        <i className="devicon-mongodb-plain colored" title="MongoDB"></i>
+                    </div>
                 </div>
-            </div>
-            <div className="skills-category">
-                <h3>Outils d'assistance & de productivité</h3>
-                <div className="skills-icons">
-                    <i className="devicon-git-plain colored" title="Git"></i>
-                    <i className="devicon-github-original colored" title="GitHub"></i>
-                    <i className="devicon-vscode-plain colored" title="VS Code"></i>
-                    🧠 ChatGPT (optimisation, génération de code, reformulation, veille technique)
+           
+                <div className="skills-content">
+                    <h3>Outils d'assistance & de productivité</h3>
+                    <div className="skills-icons">
+                        <i className="devicon-git-plain colored" title="Git"></i>
+                        <i className="devicon-github-original colored" title="GitHub"></i>
+                        <i className="devicon-vscode-plain colored" title="VS Code"></i>
+                        
+                    </div>
                 </div>
-            </div>
-            <div className="skills-category">
-                <h3>Autres compétences</h3>
-                <div className="skills-icons">
-                    <img src={accessibilityImg} alt="accessibility"/>
-                    <img src={seoImg} alt="seo"/> 
+
+                <div className="skills-content">
+                    <h3>Autres compétences</h3>
+                    <div className="skills-icons">
+                        <img src={accessibilityImg} alt="accessibility"/>
+                        <img src={seoImg} alt="seo"/> 
+                    </div>
                 </div>
             </div>   
         </section>
