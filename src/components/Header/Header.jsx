b97a96b73bdefaa0e363a292b0d20f6891f82422
Alicia <aliciajuillet0409@gmail.com>
2025-06-20 11:55:54 +0200

    portfolio-finish

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,16 @@
+import { useState } from "react"
 import logo from "../../assets/logo.webp"
 import "./Header.scss"
 
 function Header () {
+    const [isOpen, setIsOpen] = useState(false)
+
     function setIsActive(event) {
         document.querySelectorAll("nav a").forEach(element => {
             element.classList.remove('active');
         });
         event.target.classList.add('active');
+        setIsOpen(false)
     } 
 
     function logoClick() {
@@ -15,12 +19,20 @@ function Header () {
         });
         window.scrollTo({ top:0, behavior: 'smooth'})
         window.history.pushState(null, '', '/')
+        setIsOpen(false)
     }
 
     return (
         <header className="header">
             <img src={logo} alt="Alicia Juillet développeuse web" className="logo" onClick={logoClick}/>
-            <nav className="link">
+
+            <div className={`burger ${isOpen ? "open" : ""}`} onClick={() => setIsOpen(!isOpen)}>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+
+            <nav className={`link ${isOpen ? "open" : ""}`}>
                 <a href="#about" onClick={setIsActive}>
                 À propos
                 </a>
