675c60e7f40a682f77cf4fd836ebafcf0583e3ac
Alicia <aliciajuillet0409@gmail.com>
2025-11-03 10:40:35 +0100

    contact-update

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,24 +1,24 @@
-import "./Footer.scss"
+import "./Footer.scss";
 
 function Footer() {
-    return(
-        <section id="footer">
-            <h2>Me contacter</h2>
-            <div className="contact-content">
-                <div className="contact-item">
-                    <i className="fas fa-envelope"></i>
-                    <p>aliciajuillet0409@gmail.com</p>
-                </div>
-                <div className="contact-item">
-                    <i className="fa-solid fa-mobile-screen"></i>
-                    <p>+33 6 73 66 11 83</p>
-                </div>
-                <div className="contact-item">
-                    <i className="fa-brands fa-linkedin"></i>
-                    <a href="linkedin">www.linkedin.com/in/alicia-juillet</a>
-                </div>
-            </div>
-        </section>
-    )
+  return (
+    <section id="footer">
+      <h2>Me contacter</h2>
+      <div className="contact-content">
+        <div className="contact-item">
+          <i className="fas fa-envelope"></i>
+          <p>ajuillet.pro@gmail.com</p>
+        </div>
+        <div className="contact-item">
+          <i className="fa-solid fa-mobile-screen"></i>
+          <p>+33 6 73 66 11 83</p>
+        </div>
+        <div className="contact-item">
+          <i className="fa-brands fa-linkedin"></i>
+          <a href="linkedin">www.linkedin.com/in/alicia-juillet</a>
+        </div>
+      </div>
+    </section>
+  );
 }
-export default Footer
+export default Footer;
